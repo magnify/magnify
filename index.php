4ee52ab5c92d136fba4dd4140284da78c05c5f39
Brian Jensen <mail@magnify.dk>
2012-08-18 17:08:57 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <title>Webdesigner, Drupal themer, webudvikler - nyt site kommer snart!</title>
+  <link rel="shortcut icon" href="/gfx/favicon.ico" />
+  <link type="text/css" rel="stylesheet" media="screen" href="reset.css" />
+  <link type="text/css" rel="stylesheet" media="screen" href="styles.css" />
+  <link type="text/css" rel="stylesheet" media="screen" href="/chivo/stylesheet.css" />
+</head>
+<body>
+  <div class="content-wrapper">
+    <div class="content">
+      <a href="/" title="Webdesigner, Drupal themer, webudvikler  "><img src="logo.png" width="205" height="55" /></a>
+      <p>Kommer snart... Ssshh!</p>
+      <ul class="social">
+        <li><a href="http://www.twitter.com/magnifydk"><img src="twitter.png" /></a></li>
+        <li><a href="http://www.facebook.com/magnify"><img src="facebook.png" /></a></li>
+        <li><a href="http://www.linkedin.com/in/magnifydk"><img src="linkedin.png" /></a></li>
+      </ul>
+    </div>
+  </div>
+</body>
+</html>
