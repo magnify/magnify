57eee74cfda48d72334761a626d8896b90e5d256
Brian Jensen <mail@magnify.dk>
2012-08-19 22:02:38 +0200

    Testing branching

--- index.php
+++ index.php
@@ -12,13 +12,6 @@
   <div class="content-wrapper">
     <div class="content">
       <h1><a href="/" title="Webdesigner, Drupal themer, webudvikler  ">magnify.dk - Webdesigner, Drupal themer, webudvikler</a></h1>
-      <p><i>Kommer snart... Ssshh!?</i></p>
-      <p class="mail"><a href="mailto:hallo@magnify.dk">hallo@magnify.dk</a></p>
-      <ul class="social">
-        <li><a href="http://www.twitter.com/magnifydk" class="twitter">Twitter</a></li>
-        <li><a href="http://www.facebook.com/magnify" class="facebook">Facebook</a></li>
-        <li><a href="http://www.linkedin.com/in/magnifydk" class="linkedin">LinkedIn</a></li>
-      </ul>
     </div>
   </div>
 </body>
