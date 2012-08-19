fea845ae81baf9b9f0463fcc4abdfba433c44462
Brian Jensen <mail@magnify.dk>
2012-08-19 17:44:23 +0200

    Converted to sass, changed social image to one onstead of three, added email address

--- index.php
+++ index.php
@@ -5,19 +5,19 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
   <title>Webdesigner, Drupal themer, webudvikler - nyt site kommer snart!</title>
   <link rel="icon" type="image/png" href="favicon.ico">
-  <link type="text/css" rel="stylesheet" media="screen" href="reset.css" />
-  <link type="text/css" rel="stylesheet" media="screen" href="styles.css" />
+  <link type="text/css" rel="stylesheet" media="screen" href="/css/styles.css" />
   <link type="text/css" rel="stylesheet" media="screen" href="/chivo/stylesheet.css" />
 </head>
 <body>
   <div class="content-wrapper">
     <div class="content">
       <a href="/" title="Webdesigner, Drupal themer, webudvikler  "><img src="logo.png" width="205" height="55" /></a>
-      <p>Kommer snart... Ssshh!</p>
+      <p><i>Kommer snart... Ssshh!</i></p>
+      <p class="mail"><a href="mailto:hallo@magnify.dk">hallo@magnify.dk</a></p>
       <ul class="social">
-        <li><a href="http://www.twitter.com/magnifydk"><img src="twitter.png" /></a></li>
-        <li><a href="http://www.facebook.com/magnify"><img src="facebook.png" /></a></li>
-        <li><a href="http://www.linkedin.com/in/magnifydk"><img src="linkedin.png" /></a></li>
+        <li><a href="http://www.twitter.com/magnifydk" class="twitter">Twitter</a></li>
+        <li><a href="http://www.facebook.com/magnify" class="facebook">Facebook</a></li>
+        <li><a href="http://www.linkedin.com/in/magnifydk" class="linkedin">LinkedIn</a></li>
       </ul>
     </div>
   </div>
