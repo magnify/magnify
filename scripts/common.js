b231514b7e5daf2deeca95578bf279a241afea7a
Brian Jensen <hallo@magnify.dk>
2014-03-21 21:27:05 +0100

    Fixed scrolling script

--- scripts/common.js
+++ scripts/common.js
@@ -1,22 +1,11 @@
-// Add class after scrolling
-function addScrollStartClass() {
-  var el = document.getElementsByClassName('js-scroll');
+// Add class when scrolling
+function addScrollClass(scrollClass, toggle) {
+  var el = document.querySelector(toggle);
 
   if (el.classList) {
-    el[0].classList.add('is-scrolling');
+    el.classList.add(scrollClass);
   } else {
-    el[0].className += ' ' + 'is-scrolling';
-  }
-}
-
-// Add class after scrolling
-function addScrollEndClass() {
-  var el = document.getElementsByClassName('js-scroll');
-
-  if (el.classList) {
-    el[0].classList.add('is-ready');
-  } else {
-    el[0].className += ' ' + 'is-ready';
+    el.className += ' ' + scrollClass;
   }
 }
 
@@ -25,6 +14,10 @@ smoothScroll.init({
   speed: 350, // How fast to complete the scroll in milliseconds
   easing: 'easeInOutQuart', // Easing pattern to use
   updateURL: false, // Boolean. Whether or not to update the URL with the anchor hash on scroll
-  callbackBefore: addScrollStartClass,
-  callbackAfter: addScrollEndClass
+  callbackBefore: function (anchor, toggle) {
+    addScrollClass('is-scrolling', toggle);
+  },
+  callbackAfter: function (anchor, toggle) {
+    addScrollClass('is-ready', toggle);
+  },
 });
