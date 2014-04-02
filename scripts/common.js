fadd1b79fa7272ebe6e4d674064833a40bd97312
Brian Jensen <hallo@magnify.dk>
2014-04-02 18:53:24 +0200

    Added links to project images and added animation to projects when scrolling

--- scripts/common.js
+++ scripts/common.js
@@ -1,7 +1,23 @@
+// Add class when scrolling
+function addScrollClass(scrollClass, toggle) {
+  var el = document.querySelector(toggle);
+
+  if (el.classList) {
+    el.classList.add(scrollClass);
+  } else {
+    el.className += ' ' + scrollClass;
+  }
+}
+
 // Init smoothScroll
 smoothScroll.init({
   speed: 350, // How fast to complete the scroll in milliseconds
   easing: 'easeInOutQuart', // Easing pattern to use
   updateURL: true, // Boolean. Whether or not to update the URL with the anchor hash on scroll
-  offset: 40
+  callbackBefore: function (anchor, toggle) {
+    addScrollClass('is-scrolling', toggle);
+  },
+  callbackAfter: function (anchor, toggle) {
+    addScrollClass('is-ready', toggle);
+  }
 });
