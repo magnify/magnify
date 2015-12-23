63a308451add0650eb40893db11f9f539e9b50f1
Brian Jensen <hallo@magnify.dk>
2015-12-23 03:11:36 +0100

    Removed unused scripts and added some animations

--- scripts/common-min.js
+++
@@ -1,1 +0,0 @@
-function addScrollClass(a,l){var s=document.querySelector(l);s.classList?s.classList.add(a):s.className+=" "+a}smoothScroll.init({speed:350,easing:"easeInOutQuart",updateURL:!0,callbackBefore:function(a,l){addScrollClass("is-scrolling",l)},callbackAfter:function(a,l){addScrollClass("is-ready",l)}});
